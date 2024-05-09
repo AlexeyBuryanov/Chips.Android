6f3f2f14c7a61f323cbb35eaf8e3e71e769ca161
Alexey Buryanov <alexeyburyanov@gmail.com>
2024-05-09 11:09:54 +0300

    Init proj

---
+++ Chips.Android/Additions/NachosTextView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.Content;
+using Android.Runtime;
+using Android.Util;
+
+namespace Com.Hootsuite.Nachos
+{
+    [Register("Com.Hootsuite.Nachos.NachosTextView")]
+    public class NachosTextView : NachoTextView
+    {
+        public NachosTextView(Context context) 
+            : base(context)
+        {
+        }
+
+        public NachosTextView(Context context, IAttributeSet attrs) 
+            : base(context, attrs)
+        {
+        }
+
+        public NachosTextView(Context context, IAttributeSet attrs, int defStyleAttr) 
+            : base(context, attrs, defStyleAttr)
+        {
+        }
+
+        protected NachosTextView(IntPtr javaReference, JniHandleOwnership transfer) 
+            : base(javaReference, transfer)
+        {
+        }
+    }
+}
+
